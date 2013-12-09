d53ef9dd51fa3fc0dc8c816e0e460ec94680a214
Nomi Harris <nlharris@lbl.gov>
2013-12-09 11:31:18 -0800

    Removing all the code and stuff that should not have been in the docs repo

--- jnomics/bin/start-data-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH=`dirname "$0"`
-
-
-classp=`find ${SCRIPT_PATH}/../lib/*.jar ${SCRIPT_PATH}/../dist/*.jar | awk  '{ s=s$i":"} END{print s}'`
-nohup java -Djkserver_keystore=${SCRIPT_PATH}/../cert/keystore.jks -cp ${SCRIPT_PATH}/../conf:${classp} edu.cshl.schatz.jnomics.manager.server.JnomicsDataServer > /tmp/jnomics-data-server.log  &
