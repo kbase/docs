f9388b51a810b5562c4258b0a61458278a9ff145
Nomi Harris <nlharris@lbl.gov>
2013-12-10 21:21:58 -0800

    Removing a lot of stuff that shouldn't have been in docs repo

--- jnomics/bin/make_scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH=`dirname "$0"`
-
-
-classp=`find ${SCRIPT_PATH}/../lib/*.jar ${SCRIPT_PATH}/../dist/*.jar | awk  '{ s=s$i":"} END{print s}'`
-
-java -cp $classp edu.cshl.schatz.jnomics.manager.client.CreateKbaseScripts $1 $2
-
